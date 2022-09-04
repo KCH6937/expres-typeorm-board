3a1a4938926bc23ec50a7af3a512623ed997a862
KCH6937 <adasddasd12@naver.com>
2022-09-04 23:59:21 +0900

    [Feat] - 유저 Controller 구현

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import userService from '@services/user.service';
+
+const signup = async (req: Request, res: Response) => {
+  const { name } = req.body;
+
+  const result = await userService.signup(name);
+
+  if (result.isSuccess) {
+    return res.status(result.status).send(result);
+  } else {
+    console.error(result);
+    return res
+      .status(result.status)
+      .send({ status: result.status, message: result.message });
+  }
+};
+
+export default {
+  signup,
+};
