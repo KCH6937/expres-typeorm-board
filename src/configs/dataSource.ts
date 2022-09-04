8cf664e5b9827753fc203182f35eb779445dd8ff
KCH6937 <adasddasd12@naver.com>
2022-09-04 23:56:29 +0900

    [Chore] - DB 설정

---
+++ src/configs/dataSource.ts
@@ -0,0 +1,19 @@
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+
+// DB Configuration
+const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: 3306, // MySQL default port
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: true,
+  logging: true,
+  entities: ['src/entities/*.ts'],
+  migrations: ['src/migration/*.ts'],
+  subscribers: [],
+});
+
+export default AppDataSource;
